be4bcfe80d1fab7c4e74f329f9f9a5df1efce5ed
Evelio Sosa <evsosa@tamu.edu>
2021-01-30 14:22:10 -0600

    "upload two new react pages"

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,18 @@
+import React from "react";
+import { Router} from "@reach/router";
+import Box from "@material-ui/core/Box";
+import { UserPage } from "./Pages/UserPage";
+import { OrganizationPage } from "./Pages/OrganizationPage";
 import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <Box className="App">
+      <Router>
+        <UserPage path="/user" />
+        <OrganizationPage path="/organization" />
+      </Router>
+    </Box>
   );
 }
 
---
+++ frontend/src/Pages/UserPage.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+
+export const UserPage = () => {
+    return(
+        <Box>
+            <Typography>Hello, User!</Typography>
+        </Box>
+    );
+};
+
