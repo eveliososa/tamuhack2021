fe738e86632d848a0c09cb09e69329988f6937bc
Evelio Sosa <evsosa@tamu.edu>
2021-01-30 16:29:28 -0600

    "add main page"

--- frontend/src/Pages/UserPage.js
+++ frontend/src/Pages/UserPage.js
@@ -2,12 +2,14 @@ import * as React from "react";
 import Box from "@material-ui/core/Box";
 import Typography from "@material-ui/core/Typography";
 import { UserCharityComponent } from "../Components/UserCharityComponent/UserCharityComponent";
+import { UserMainComponent } from "../Components/UserMainComponent/UserMainComponent";
 
 export const UserPage = () => {
     return(
         <Box>
             <Typography>Hello, User!</Typography>
             <UserCharityComponent/>
+            <UserMainComponent/>
         </Box>
     );
 };
