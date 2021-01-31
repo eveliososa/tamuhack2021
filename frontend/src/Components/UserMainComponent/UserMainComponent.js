a3c73a38aec1e1460cbe8ebe977e54efa8e67863
Evelio Sosa <evsosa@tamu.edu>
2021-01-31 09:59:52 -0600

    "commit files"

--- frontend/src/Components/UserMainComponent/UserMainComponent.js
+++ frontend/src/Components/UserMainComponent/UserMainComponent.js
@@ -5,67 +5,21 @@ import { Typography } from "@material-ui/core";
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Avatar from '@material-ui/core/Avatar'
 
-export const UserMainComponent = () => {
-    const [data, setData] = React.useState({
-        "userName" : "James",
-        "userAvatar": "dog.PNG",
-        "organizations" : { 
-            "1" : {
-                "name": "Organization 1",
-                "amountDonated": "3,000",
-                "goalPercentage": 22
-                
-            },
-            "2" : {
-                "name": "Organization 1",
-                "amountDonated": "3,000",
-                "goalPercentage": 40
-                
-            },
-            "3" : {
-                "name": "Organization 1",
-                "amountDonated": "3,000",
-                "goalPercentage": 80
-                
-            }
-        }
-    })
-   
-
+export const UserMainComponent = ({
+    userData, data, userOrgData,
+}) => {
     return(
-
-        <Box>  
-                    <Typography>Welcome {data["userName"]} to goCharity</Typography>
-                   
-                    <Avatar alt="userAvatar" src={data["userAvatar"]} />
-            <Box>
-                <Box className="LinearContainer">
-                    <LinearProgress  variant="determinate" value={data["organizations"]["1"]["goalPercentage"]} />
-                </Box>
-                    <Typography>{data["organizations"]["1"]["goalPercentage"]}%</Typography>
-                    <Typography>Total $ {data["organizations"]["1"]["amountDonated"]}</Typography>
-                    <Typography>{data["organizations"]["1"]["name"]}</Typography>
+        <Box className="userPageContainer">  
+            <Typography className="userPageTitle">Welcome {userData["first_name"]} {userData["last_name"]}!</Typography>
+            <Avatar className="userPageAvatar">{userData["first_name"][0]}</Avatar>
+            <Box display={userOrgData['subscriptions'] === undefined ? "none": "block"}>
             </Box>
-            <Box>
-                <Box className="LinearContainer">
-                    <LinearProgress  variant="determinate" value={data["organizations"]["2"]["goalPercentage"]} />
-                </Box>
-                    <Typography>{data["organizations"]["2"]["goalPercentage"]}%</Typography>
-                    <Typography>Total $ {data["organizations"]["2"]["amountDonated"]}</Typography>
-                    <Typography>{data["organizations"]["2"]["name"]}</Typography>
+            <Box display={userOrgData['subscriptions'] === undefined ? "block": "none"}>
+                <Typography>You have no organizations selected.</Typography>
             </Box>
-            <Box>
-                <Box className="LinearContainer">
-                    <LinearProgress  variant="determinate" value={data["organizations"]["3"]["goalPercentage"]} />
-                </Box>
-                    <Typography>{data["organizations"]["3"]["goalPercentage"]}%</Typography>
-                    <Typography>Total $ {data["organizations"]["3"]["amountDonated"]}</Typography>
-                    <Typography>{data["organizations"]["3"]["name"]}</Typography>
-            </Box>
-                
-
         </Box>
       
-    );}
+    );
+}
 
    
--- frontend/src/Pages/UserPage/UserPage.js
+++ frontend/src/Pages/UserPage/UserPage.js
@@ -5,12 +5,15 @@ import Box from "@material-ui/core/Box";
 import Button from "@material-ui/core/Button";
 import { UserCharityComponent } from "../../Components/UserCharityComponent/UserCharityComponent";
 import { UserMainComponent } from "../../Components/UserMainComponent/UserMainComponent";
+import { NavigationComponent } from "../../Components/NavigationComponent/NavigationComponent";
 
 export const UserPage = () => {
     const { username } = useParams();
+    const [loading, setLoading] = React.useState(true);
 
     const [userData, setUserData] = React.useState([]);
     const [data, setData] = React.useState({});
+    const [userOrgData, setUserOrgData] =  React.useState({});
 
     React.useEffect(() => {
         fetch(`http://127.0.0.1:5000/api/user/${username}`, {
@@ -22,6 +25,7 @@ export const UserPage = () => {
                 return response.json();
             }
         }).then(data => {
+            console.log(data);
             setUserData(data);
             getOrganizations();
         })
@@ -33,7 +37,6 @@ export const UserPage = () => {
             method: "GET",
             headers: {"Content-Type":"application/json"}
         }).then(response => {
-            console.log(response)
             if(response.ok){
                 return response.json();
             } else{
@@ -41,9 +44,30 @@ export const UserPage = () => {
             }
         }).then(data => {
             if(data!==null){
+                console.log(data);
                 setData(data);
+                getUserOrganizations();
             }
-        });
+        }).finally(() => setLoading(false));
+    };
+
+    const getUserOrganizations = () => {
+        fetch(`http://127.0.0.1:5000//api/userOrganizations/${username}`, {
+            crossDomain: true,
+            method: "GET",
+            headers: {"Content-Type":"application/json"}
+        }).then(response => {
+            if(response.ok){
+                return response.json();
+            } else{
+                return null;
+            }
+        }).then(data => {
+            if(data!==null){
+                console.log(data);
+                setUserOrgData(data);
+            }
+        })
     };
 
     const pageOptions = ["Home", "Charities"];
@@ -53,18 +77,25 @@ export const UserPage = () => {
         setPage(page);
     }
 
+    if(loading){
+        return(
+            <Box></Box>
+        );
+    };
+
     return(
-        <Box>
+        <Box className="userPageContainer">
+            <NavigationComponent/>
             <Box className="navigationContainer">
                 {pageOptions.map((pageName) => (
                     <Button variant={page === pageName ? "contained" : "outlined"} color="primary" key={pageName} className="navigationButton" onClick={() => handlePage(pageName)}>{pageName}</Button>
                 ))}
             </Box>
             <Box display={page === pageOptions[0] ? "block" : "none"}>
-                <UserMainComponent/>
+                <UserMainComponent userData={userData} data={data} userOrgData={userOrgData}/>
             </Box>
             <Box display={page === pageOptions[1] ? "block" : "none"}>
-                <UserCharityComponent/>
+                <UserCharityComponent data={data} userOrgData={userOrgData}/>
             </Box>
         </Box>
     );
